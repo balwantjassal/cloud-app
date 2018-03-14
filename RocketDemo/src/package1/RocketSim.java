5bb2d379eff9a1bc67a0c46987475cfd8732ca48
bob <balwantjassal@gmail.com>
2018-03-14 14:55:10 +0530

    Testing

---
+++ RocketDemo/src/package1/RocketSim.java
@@ -0,0 +1,10 @@
+package package1;
+import java.io.*;
+public class RocketSim {
+	public static void main(String[] args) {
+		System.out.println("Testing Rocket Sim1");
+		BufferedReader reader= new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Testing In Phase");
+	}
+
+}
