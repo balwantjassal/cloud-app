1db869166b698a90e04cb2cedb44fbf8697b55bf
bob <bob@vhcalnplci>
2018-03-14 20:53:10 +0530

    Commit Done

--- RocketDemo/src/package1/RocketSim.java
+++ RocketDemo/src/package1/RocketSim.java
@@ -4,7 +4,8 @@ public class RocketSim {
 	public static void main(String[] args) {
 		System.out.println("Testing Rocket Sim1");
 		BufferedReader reader= new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Testing In Phase");
+		System.out.println("Testing In Phase2");
+		System.out.println("Testing In Phase2");
 	}
 
 }
