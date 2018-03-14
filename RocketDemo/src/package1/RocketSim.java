0ec2af4500444bf4561d19c3226bdb62e504c765
balwantjassal <34181138+balwantjassal@users.noreply.github.com>
2018-03-14 15:52:12 +0530

    RocketSim.java
    Hello

--- RocketDemo/src/package1/RocketSim.java
+++ RocketDemo/src/package1/RocketSim.java
@@ -5,7 +5,7 @@ public class RocketSim {
 		System.out.println("Testing Rocket Sim1");
 		BufferedReader reader= new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Testing Demo from server");
-		System.out.println("Testing Demo from Server Hurraah");
+		System.out.println("Testing Demo fkrom Server Hurraah");
 	}
 
 }
