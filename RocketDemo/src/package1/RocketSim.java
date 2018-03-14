f33c78201be4fd0af9cb66b6fc27a72d51530f50
bob <bob@vhcalnplci>
2018-03-14 21:11:04 +0530

    Test

--- RocketDemo/src/package1/RocketSim.java
+++ RocketDemo/src/package1/RocketSim.java
@@ -4,8 +4,8 @@ public class RocketSim {
 	public static void main(String[] args) {
 		System.out.println("Testing Rocket Sim1");
 		BufferedReader reader= new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Testing In Phase2");
-		System.out.println("Testing In Phase2");
+		System.out.println("Testing Phase Two");
+		System.out.println("Testing Phase One");
 	}
 
 }
