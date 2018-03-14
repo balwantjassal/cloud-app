f5ab2df21a29fc90992185f815ddfc7057af5ecc
balwantjassal <34181138+balwantjassal@users.noreply.github.com>
2018-03-14 15:42:18 +0530

    Update RocketSim.java

--- RocketDemo/src/package1/RocketSim.java
+++ RocketDemo/src/package1/RocketSim.java
@@ -4,8 +4,8 @@ public class RocketSim {
 	public static void main(String[] args) {
 		System.out.println("Testing Rocket Sim1");
 		BufferedReader reader= new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Testing Phase Two");
-		System.out.println("Testing Phase One");
+		System.out.println("Testing Demo from server");
+		System.out.println("Testing Demo from Server Hurraah");
 	}
 
 }
